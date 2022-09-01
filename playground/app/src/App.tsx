07d3872448e741c34ff41f49e80fc22de46f2832
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-09-01 17:57:01 +0200

    fmt

--- playground/app/src/App.tsx
+++ playground/app/src/App.tsx
@@ -1,49 +1,45 @@
-import { options } from "@zeitgeistpm/api";
-import { ApiPromise, WsProvider } from "@polkadot/api";
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { options } from '@zeitgeistpm/api'
+import { ApiPromise, WsProvider } from '@polkadot/api'
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
-import * as Indexer from "@zeitgeistpm/indexer";
-import {
-  AssetOrderByInput,
-  MarketOrderByInput,
-  PoolOrderByInput,
-} from "@zeitgeistpm/indexer";
+import * as Indexer from '@zeitgeistpm/indexer'
+import { AssetOrderByInput, MarketOrderByInput, PoolOrderByInput } from '@zeitgeistpm/indexer'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   useEffect(() => {
-    const provider = new WsProvider("wss://bsr.zeitgeist.pm");
-    const api = ApiPromise.create(options({ provider }));
+    const provider = new WsProvider('wss://bsr.zeitgeist.pm')
+    const api = ApiPromise.create(options({ provider }))
     const indexer = Indexer.create({
-      endpoint: "https://processor.zeitgeist.pm/graphql",
-    });
+      endpoint: 'https://processor.zeitgeist.pm/graphql',
+    })
 
     indexer.markets({
       order: MarketOrderByInput.IdAsc,
       where: {},
-    });
+    })
 
     indexer.assets({
       order: AssetOrderByInput.IdAsc,
       where: {},
-    });
+    })
 
     indexer.pools({
       order: PoolOrderByInput.CreatedAtAsc,
       where: {
         poolId_eq: 1,
       },
-    });
+    })
 
-    api.then((api) => {
-      api.query.predictionMarkets.disputes(null).then((markets) => {
-        console.log(markets.toHuman());
-      });
-    });
-  }, []);
+    api.then(api => {
+      api.query.predictionMarkets.disputes(null).then(markets => {
+        console.log(markets.toHuman())
+      })
+    })
+  }, [])
 
   return (
     <div className="App">
@@ -57,18 +53,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
