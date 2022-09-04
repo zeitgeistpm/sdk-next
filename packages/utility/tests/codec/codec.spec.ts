96a04ace8a490dac7dcd9737444bcea946545007
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-09-04 19:11:29 +0200

    test for codec compose

--- packages/utility/tests/codec/codec.spec.ts
+++ packages/utility/tests/codec/codec.spec.ts
@@ -1,27 +1,50 @@
 import { describe, expect, it } from 'vitest'
-import { codec } from '../../src/codec'
+import { codec, compose } from '../../src/codec'
 import { throws } from '../../src/error'
 
 describe('codec', () => {
-  it('creating a codec from functions that throw', () => {
-    const c = codec<string, boolean>({
-      encode: str => {
-        if (str === 'correct') {
-          return true
-        }
-        throw new Error('incorrect')
-      },
-      decode: bool => {
-        if (bool) {
-          return 'correct'
-        }
-        throw new Error('incorrect')
-      },
-    })
+  describe('codec', () => {
+    it('creating a codec from functions that throw', () => {
+      const c = codec<string, boolean>({
+        encode: str => {
+          if (str === 'correct') {
+            return true
+          }
+          throw new Error('incorrect')
+        },
+        decode: bool => {
+          if (bool) {
+            return 'correct'
+          }
+          throw new Error('incorrect')
+        },
+      })
 
-    expect(c.decode(true).unrightOr(throws)).toBe('correct')
-    expect(c.decode(false).unleftOr(throws)).toEqual(new Error('incorrect'))
-    expect(c.encode('correct').unrightOr(throws)).toBe(true)
-    expect(c.encode('incorrect').unleftOr(throws)).toEqual(new Error('incorrect'))
+      expect(c.decode(true).unrightOr(throws)).toBe('correct')
+      expect(c.decode(false).unleftOr(throws)).toEqual(new Error('incorrect'))
+      expect(c.encode('correct').unrightOr(throws)).toBe(true)
+      expect(c.encode('incorrect').unleftOr(throws)).toEqual(
+        new Error('incorrect'),
+      )
+    })
+  })
+
+  describe('compose', () => {
+    it('should compose two codecs', () => {
+      const a = codec<string, string>({
+        encode: str => `${str}-added`,
+        decode: str => str.replace('-added', ''),
+      })
+
+      const b = codec<string, string>({
+        encode: str => str.toUpperCase(),
+        decode: str => str.toLowerCase(),
+      })
+
+      const c = compose(a, b)
+
+      expect(c.encode('foo').unrightOr(throws)).toBe('FOO-ADDED')
+      expect(c.decode('FOO-ADDED').unrightOr(throws)).toBe('foo')
+    })
   })
 })
