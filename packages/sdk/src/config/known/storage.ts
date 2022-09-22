382710130f872855b4ba589bc905feaf2d6e1864
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-09-22 23:07:24 +0200

    move duplicate code

--- packages/sdk/src/config/known/storage.ts
+++ packages/sdk/src/config/known/storage.ts
@@ -1,6 +1,10 @@
 import { IPFS } from '@zeitgeistpm/web3.storage'
 import { MarketMetadata } from '../../model/market/meta/types'
 
+/**
+ * Default IPFS metadata storage for the zeitgeist ecosystem.
+ * @typeof IPFS.storage<MarketMetadata>
+ */
 export const zeitgeistIpfs = IPFS.storage<MarketMetadata>({
   node: { url: 'http://ipfs.zeitgeist.pm:5001' },
   cluster: {
