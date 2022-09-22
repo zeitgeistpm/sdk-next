b09bb13081accca449319bf9b06dd42b79967417
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-09-22 22:51:42 +0200

    Update index.ts

--- packages/sdk/src/context/index.ts
+++ packages/sdk/src/context/index.ts
@@ -1,1 +1,12 @@
+import { FullContext, IndexerContext, RpcContext } from './types'
+
 export * from './types'
+
+export const isFullContext = (config?: unknown): config is FullContext =>
+  isRpcContext(config) && isIndexerContext(config)
+
+export const isRpcContext = (config?: unknown): config is RpcContext =>
+  Boolean(config && typeof config === 'object' && config !== null && 'api' in config)
+
+export const isIndexerContext = (config?: unknown): config is IndexerContext =>
+  Boolean(config && typeof config === 'object' && config !== null && 'indexer' in config)
