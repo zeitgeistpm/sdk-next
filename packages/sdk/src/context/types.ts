4648300b4210d55e15beca45ff23a46b1e649b9c
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-09-22 23:03:46 +0200

    comments

--- packages/sdk/src/context/types.ts
+++ packages/sdk/src/context/types.ts
@@ -4,16 +4,28 @@ import type { ZeitgeistIndexer } from '@zeitgeistpm/indexer'
 import type { MetadataStorage } from '@zeitgeistpm/web3.storage'
 import type { MarketMetadata } from '../model/market/meta/types'
 
+/**
+ * Union type that can be either rpc, indexer or full context.
+ */
 export type Context = FullContext | (RpcContext | IndexerContext)
 
+/**
+ * Zeitgeist SDK context with both rpc and indexer features enabled.
+ */
 export type FullContext = RpcContext & IndexerContext
 
+/**
+ * Zeitgeist SDK context with rpc and storage features enabled.
+ */
 export type RpcContext = {
   api: ApiPromise
   provider: WsProvider
   storage: MetadataStorage<MarketMetadata, CID>
 }
 
+/**
+ * Zeitgeist SDK context with indexer features enabled.
+ */
 export type IndexerContext = {
   indexer: ZeitgeistIndexer
 }
