40221c38083ed22a0548efb7ba304cbb9f499c91
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-09-23 13:59:45 +0200

    work

--- playground/app/src/App.tsx
+++ playground/app/src/App.tsx
@@ -1,8 +1,14 @@
-import { web3Enable } from '@polkadot/extension-dapp'
+import { web3Enable, web3FromAddress } from '@polkadot/extension-dapp'
 import { batterystation, builder, Context, isIndexedSdk, isRpcSdk, Sdk } from '@zeitgeistpm/sdk'
+import { throws } from '@zeitgeistpm/utility/dist/error'
 import { useEffect, useState } from 'react'
 
-import { IndexedMarket, isRpcMarket, RpcMarket } from '@zeitgeistpm/sdk/dist/model/types'
+import {
+  CreateMarketWithPoolParams,
+  IndexedMarket,
+  isRpcMarket,
+  RpcMarket,
+} from '@zeitgeistpm/sdk/dist/model/types'
 
 const App: React.FC = () => {
   const [sdk, setSdk] = useState<Partial<Sdk<Context>>>()
@@ -15,7 +21,10 @@ const App: React.FC = () => {
 
   useEffect(() => {
     web3Enable('sdkv2')
-    builder(batterystation()).subscribe(sdk => {
+    builder({
+      ...batterystation(),
+      provider: 'ws://127.0.0.1:9944',
+    }).subscribe(sdk => {
       setSdk(sdk)
     })
   }, [])
@@ -26,8 +35,59 @@ const App: React.FC = () => {
     }
   }, [sdk])
 
+  const createMarket = async () => {
+    if (!isRpcSdk(sdk)) return
+
+    const address = 'dE2cVL9QAgh3MZEK3ZhPG5S2YSqZET8V1Qa36epaU4pQG4pd8'
+    const { signer } = await web3FromAddress(address)
+
+    const baseWeight = (1 / 2) * 10 * 10 ** 10
+
+    const params: CreateMarketWithPoolParams = {
+      signer: {
+        address,
+        signer,
+      },
+      disputeMechanism: { Authorized: address },
+      marketType: { Scalar: [1, 2] },
+      oracle: address,
+      period: { Timestamp: [Date.now(), Date.now() + 60 * 60 * 24 * 1000 * 2] },
+      metadata: {
+        question: 'Testing Jørn',
+        description: 'just a test market, can be removed',
+        slug: 'yornaath_test',
+        categories: [
+          {
+            name: 'yes',
+            ticker: 'Y',
+          },
+          {
+            name: 'no',
+            ticker: 'N',
+          },
+        ],
+      },
+      pool: {
+        amount: `${300 * 10 ** 10}`,
+        swapFee: `${1000}`,
+        weights: [`${baseWeight}`, `${baseWeight}`],
+      },
+    }
+
+    const result = await sdk.model.markets.create(params)
+
+    const {
+      market: [marketId, market],
+      pool: [poolId, pool],
+    } = result.extract().unrightOr(throws)
+
+    console.log(`created: market ${marketId}`, market.toHuman())
+    console.log(`created: pool ${poolId}`, pool.toHuman())
+  }
+
   return (
     <div style={{ display: 'grid', columnGap: '50px' }}>
+      <button onClick={createMarket}>test creating market without pool</button>
       {markets.map((market, index) => (
         <Market key={index} market={market} />
       ))}
@@ -48,56 +108,6 @@ const Market = (props: { market: RpcMarket | IndexedMarket }) => {
 }
 
 export default App
-
-//   const createMarket = async () => {
-//     if (!isRpcSdk(sdk)) return
-
-//     const address = 'dE2cVL9QAgh3MZEK3ZhPG5S2YSqZET8V1Qa36epaU4pQG4pd8'
-//     const { signer } = await web3FromAddress(address)
-
-//     const baseWeight = (1 / 2) * 10 * 10 ** 10
-
-//     const params: CreateMarketWithPoolParams = {
-//       signer: {
-//         address,
-//         signer,
-//       },
-//       disputeMechanism: { Authorized: address },
-//       marketType: { Scalar: [1, 2] },
-//       oracle: address,
-//       period: { Timestamp: [Date.now(), Date.now() + 60 * 60 * 24 * 1000 * 2] },
-//       metadata: {
-//         question: 'Testing Jørn',
-//         description: 'just a test market, can be removed',
-//         slug: 'yornaath_test',
-//         categories: [
-//           {
-//             name: 'yes',
-//             ticker: 'Y',
-//           },
-//           {
-//             name: 'no',
-//             ticker: 'N',
-//           },
-//         ],
-//       },
-//       pool: {
-//         amount: `${300 * 10 ** 10}`,
-//         swapFee: `${1000}`,
-//         weights: [`${baseWeight}`, `${baseWeight}`],
-//       },
-//     }
-
-//     const result = await sdk.model.markets.create(params)
-
-//     const {
-//       market: [marketId, market],
-//       pool: [poolId, pool],
-//     } = result.extract().unrightOr(throws)
-
-//     console.log(`created: market ${marketId}`, market.toHuman())
-//     console.log(`created: pool ${poolId}`, pool.toHuman())
-//   }
 
 //   return (
 //     <div className="App">
--- playground/app/src/main.tsx
+++ playground/app/src/main.tsx
@@ -1,6 +1,5 @@
 import 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
