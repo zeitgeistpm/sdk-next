f7a9a13aade2f85e0c86f77459169d9078e288a2
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-09-27 00:52:18 +0200

    Update index.ts

--- packages/sdk/src/model/assets/functions/prices/index.ts
+++ packages/sdk/src/model/assets/functions/prices/index.ts
@@ -2,7 +2,9 @@ import { IndexerContext, RpcContext } from '../../../../context'
 import { PricesQuery } from './types'
 
 export const rpcPrices = async (context: RpcContext, query: PricesQuery) => {
-  //context.api.rpc.swaps.getAssetSpotPricesForPool()
+  //context.api.rpc.swaps.getAssetSpotPricesForPool() // !blocked
 }
 
-export const indexedPrices = async (context: IndexerContext, query: PricesQuery) => {}
+export const indexedPrices = async (context: IndexerContext, query: PricesQuery) => {
+  //!TODO: use indexed historicalAssets
+}
