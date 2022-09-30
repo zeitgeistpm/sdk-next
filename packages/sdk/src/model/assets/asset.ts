7c6e93aac1b715d3d977d773182c603c30c47771
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-09-30 17:08:07 +0200

    Update asset.ts

--- packages/sdk/src/model/assets/asset.ts
+++ packages/sdk/src/model/assets/asset.ts
@@ -11,6 +11,11 @@ export type ZtgAssetId = Infer<typeof IOZtgAssetId>
 export type PoolShareAssetId = Infer<typeof IOPoolShareAssetId>
 export type AssetId = Infer<typeof IOAssetId>
 
+/**
+ * Io types for the asset ids.
+ * Usefull for decoding from a string or json object.
+ */
+
 export const IOScalarPosition = union([literal('Short'), literal('Long')])
 
 export const IOCategoricalIndex = tuple([number(), number()])
@@ -39,9 +44,21 @@ export const IOAssetId = union([
   IOPoolShareAssetId,
 ])
 
+/**
+ * Get asset index of a scalar asset, short being 0 and long being 1
+ *
+ * @param scalarAssetId ScalarAssetId
+ * @returns number
+ */
 export const getScalarIndexOf = (scalarAssetId: ScalarAssetId): number =>
   scalarAssetId.scalarOutcome[1] == 'Short' ? 0 : 1
 
+/**
+ * Get the asset index of an AssetId, will return index only for scalar and categorical assets.
+ *
+ * @param assetId AssetId
+ * @returns number | null
+ */
 export const getIndexOf = (assetId: AssetId): number | null =>
   IOCategoricalAssetId.is(assetId)
     ? assetId.categoricalOutcome[1]
