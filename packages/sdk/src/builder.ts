bae0cdd5f3e11dab9ae447e1c880897f78d7d3f2
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-10-02 21:04:29 +0200

    fmt

--- packages/sdk/src/builder.ts
+++ packages/sdk/src/builder.ts
@@ -1,11 +1,10 @@
 import { assign } from '@zeitgeistpm/utility/dist/observable/operators'
-import { MarketMetadata } from 'meta/market'
 import { Metadata } from 'meta/types'
 import { from, merge, Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
 import { createIndexerContext, createRpcContext } from './create'
 import * as Model from './model'
-import { FullConfig, FullContext, Sdk, Context } from './types'
+import { FullConfig, FullContext, Sdk } from './types'
 
 /**
  * Initialize the indexer and rpc concurrently and emit partially applied intances of the Sdk.
