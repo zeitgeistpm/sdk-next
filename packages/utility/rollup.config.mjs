0f7cf93d80f797f45873c35c0717863b63e70a5c
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-10-21 13:36:11 +0200

    work

--- packages/utility/rollup.config.mjs
+++ packages/utility/rollup.config.mjs
@@ -1,8 +1,11 @@
 import typescript from 'rollup-plugin-typescript2'
 import commonjs from '@rollup/plugin-commonjs'
+import glob from 'glob'
+
+const folders = glob.sync('./src/*')
 
 export default {
-  input: './src',
+  input: folders,
   output: [
     {
       dir: './dist/esm/',
