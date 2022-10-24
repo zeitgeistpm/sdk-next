9b980b8cbac98c5cc34485b3f9bd5ca0aa385fc1
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-10-24 11:50:42 +0200

    work

---
+++ packages/utility/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup'
+import glob from 'glob'
+
+const folders = glob.sync('./src/**/*.ts')
+
+export default defineConfig({
+  entry: folders,
+  dts: true,
+  format: ['cjs', 'esm'],
+  ignoreWatch: ['*.test.ts'],
+  target: 'node16',
+  clean: true,
+})
