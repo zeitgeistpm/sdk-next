d67642910739799b619e18657b0ff539d9d97b12
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-10-24 12:01:57 +0200

    work

---
+++ packages/utility/scripts/generate_exports.mjs
@@ -0,0 +1,39 @@
+import glob from 'glob'
+import path from 'path'
+import fs from 'fs'
+
+const folders = glob.sync('./src/*')
+
+const pckexports = folders.reduce(
+  (exp, folder) => {
+    const modulename = path.basename(folder)
+    return {
+      ...exp,
+      [`./dist/${modulename}`]: {
+        types: `./dist/${modulename}/index.d.ts`,
+        require: `./dist/${modulename}/index.cjs`,
+        import: `./dist/${modulename}/index.js`,
+      },
+    }
+  },
+  {
+    // './dist/*': {
+    //   require: './dist/cjs/*',
+    //   import: './dist/esm/*',
+    // },
+  },
+)
+
+const pckjson = JSON.parse(fs.readFileSync('./package.json'))
+
+fs.writeFileSync(
+  './package.json',
+  JSON.stringify(
+    {
+      ...pckjson,
+      exports: pckexports,
+    },
+    undefined,
+    2,
+  ),
+)
