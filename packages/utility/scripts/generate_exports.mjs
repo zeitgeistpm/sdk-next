e63517a91808feff949337e700e29a2e3e18874a
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-11-09 11:50:00 +0100

    Update generate_exports.mjs

--- packages/utility/scripts/generate_exports.mjs
+++ packages/utility/scripts/generate_exports.mjs
@@ -2,27 +2,24 @@ import glob from 'glob'
 import path from 'path'
 import fs from 'fs'
 
+/**
+ * Generate exports fields in the package json for cjs and esm modules.
+ * Creates on field for every folder in the src folder.
+ */
+
 const folders = glob.sync('./src/*')
 
-const pckexports = folders.reduce(
-  (exp, folder) => {
-    const modulename = path.basename(folder)
-    return {
-      ...exp,
-      [`./dist/${modulename}`]: {
-        types: `./dist/${modulename}/index.d.ts`,
-        require: `./dist/${modulename}/index.cjs`,
-        import: `./dist/${modulename}/index.mjs`,
-      },
-    }
-  },
-  {
-    // './dist/*': {
-    //   require: './dist/cjs/*',
-    //   import: './dist/esm/*',
-    // },
-  },
-)
+const pckexports = folders.reduce((exp, folder) => {
+  const modulename = path.basename(folder)
+  return {
+    ...exp,
+    [`./dist/${modulename}`]: {
+      types: `./dist/${modulename}/index.d.ts`,
+      require: `./dist/${modulename}/index.cjs`,
+      import: `./dist/${modulename}/index.mjs`,
+    },
+  }
+}, {})
 
 const pckjson = JSON.parse(fs.readFileSync('./package.json'))
 
