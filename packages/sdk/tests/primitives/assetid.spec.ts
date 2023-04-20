344de0efad0e8a8af19c6194b45f9b66d299ae72
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2023-04-20 11:00:25 +0200

    test for lowercase keys like in indexed data

--- packages/sdk/tests/primitives/assetid.spec.ts
+++ packages/sdk/tests/primitives/assetid.spec.ts
@@ -21,5 +21,24 @@ describe('sdk.primitives.AssetId', () => {
         AssetId.IOPoolShareAssetId.is(AssetId.parseAssetId('{"PoolShare":1}').unwrap()),
       ).toBe(true)
     })
+
+    it('should be able to parse asset ids starting with lowercase keys as they are in the indexer data', () => {
+      expect(AssetId.IOZtgAssetId.is(AssetId.parseAssetId('{"ztg":null}').unwrap())).toBe(
+        true,
+      )
+      expect(
+        AssetId.IOCategoricalAssetId.is(
+          AssetId.parseAssetId('{"categoricalOutcome":[1,2]}').unwrap(),
+        ),
+      ).toBe(true)
+      expect(
+        AssetId.IOScalarAssetId.is(
+          AssetId.parseAssetId('{"scalarOutcome":[1,"Long"]}').unwrap(),
+        ),
+      ).toBe(true)
+      expect(
+        AssetId.IOPoolShareAssetId.is(AssetId.parseAssetId('{"poolShare":1}').unwrap()),
+      ).toBe(true)
+    })
   })
 })
