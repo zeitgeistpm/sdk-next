9872b1f1ae8bb4c1d64afebe9530ebfd0d13de61
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2023-09-20 15:39:37 +0200

    work

--- playground/examples/src/restore-metadata/index.ts
+++ playground/examples/src/restore-metadata/index.ts
@@ -1,7 +1,9 @@
 import {
   FullContext,
   IndexerContext,
+  MarketMetadata,
   RpcContext,
+  RpcMarket,
   Sdk,
   mainnetIndexer,
   mainnetRpc,
@@ -10,29 +12,38 @@ import { isCodec, isNumber } from '@polkadot/util'
 import { create, mainnet } from '@zeitgeistpm/sdk'
 import CID from 'cids'
 import { createClient } from 'redis'
+import { poll } from './poll'
+import { IOption, option, none } from '@zeitgeistpm/utility/dist/option'
 
-const client = createClient({
-  url: 'redis://:redis@processor.rpc-0.zeitgeist.pm:6379',
+// const client = createClient({
+//   url: 'redis://:redis@processor.rpc-0.zeitgeist.pm:6379',
+// })
+
+const rpcSdk: Sdk<RpcContext> = await create(mainnetRpc())
+const idxSdk: Sdk<IndexerContext> = await create(mainnetIndexer())
+
+const fetchMetadata = (market: any) => {
+  return new Promise<IOption<MarketMetadata>>(async (resolve, reject) => {
+    const timer = setTimeout(() => {
+      resolve(option(none()))
+    }, 2000)
+    const hash = isCodec(market.metadata) ? market.metadata.toHex() : market.metadata
+    const cid = new CID('f0155' + hash.slice(2))
+    const id = { __meta: 'markets', cid: cid } as any
+    const metadata = await rpcSdk.storage.of('markets').get(id)
+    clearTimeout(timer)
+    resolve(metadata)
+  })
+}
+
+const rpcMarkets = await rpcSdk.model.markets.list()
+
+console.log(rpcMarkets.length)
+
+rpcMarkets.forEach(async market => {
+  const metadata = await fetchMetadata(market)
+  if (metadata.isNone()) {
+    const indexedMarket = await idxSdk.model.markets.get(market.marketId)
+    // reconstruct and chech hash
+  }
 })
-
-const a = await client.get('WAt')
-
-console.log(a)
-
-// const sdkIndexed: Sdk<IndexerContext> = await create(mainnetIndexer())
-// const sdkRpc: Sdk<RpcContext> = await create(mainnetRpc())
-
-// const indexedMarket = (await sdkIndexed.model.markets.get(141)).unwrap()
-// const rpcMarket = (await sdkRpc.model.markets.get(141)).unwrap()
-
-// const fetchMetadata = async (market: any) => {
-//   const hash = isCodec(market.metadata) ? market.metadata.toHex() : market.metadata
-//   const cid = new CID('f0155' + hash.slice(2))
-//   const id = { __meta: 'markets', cid: cid } as any
-//   const metadata = await sdkRpc.storage.of('markets').get(id)
-//   return market.marketId, metadata.unwrap()?.question
-// }
-
-// console.log('f0155' + indexedMarket?.metadata.slice(2))
-// console.log('indexed', indexedMarket?.metadata, await fetchMetadata(indexedMarket))
-// console.log('rpc', rpcMarket?.metadata.toHex(), await fetchMetadata(rpcMarket))
