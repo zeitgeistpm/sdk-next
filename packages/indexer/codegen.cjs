e1799f25bcdcfc3a0106fac0d545190729a90c33
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2024-01-23 14:08:02 +0530

    Reset url

--- packages/indexer/codegen.cjs
+++ packages/indexer/codegen.cjs
@@ -1,5 +1,5 @@
 const endpoint = process.argv.includes('--bsr')
-  ? 'https://processor.zeitgeist.pm/graphql'
+  ? 'https://processor.bsr.zeitgeist.pm/graphql'
   : process.argv.includes('--mainnet')
   ? 'https://processor.rpc-0.zeitgeist.pm/graphql'
   : null
